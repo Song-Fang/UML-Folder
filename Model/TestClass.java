e1354a537bcc7c4af0220b238284d1e96c16e16f
SongFang <sof10@pitt.edu>
2020-08-19 13:32:39 -0400

    Signed-off-by: SongFang <sof10@pitt.edu>

---
+++ Model/TestClass.java
@@ -0,0 +1,67 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class TestClass {
+
+    /**
+     * Default constructor
+     */
+    public TestClass() {
+    }
+
+    /**
+     * 
+     */
+    public String publicAttribute;
+
+    /**
+     * 
+     */
+    private int  privateAttribute = 1;
+
+    /**
+     * 
+     */
+    protected double protectedAttribute;
+
+    /**
+     * 
+     */
+    boolean packageAttribute;
+
+    /**
+     * @param Parameter1 
+     * @return
+     */
+    public String publicMethod(int Parameter1) {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * 
+     */
+    private void privateMethod() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    void defaultMethod() {
+        // TODO implement here
+    }
+
+    /**
+     * @param Parameter1 
+     * @return
+     */
+    protected void protectedMethod(void Parameter1) {
+        // TODO implement here
+        return null;
+    }
+
+}
